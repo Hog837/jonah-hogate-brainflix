322c6631d515be0e98b1498e57085cf1d2661ef3
Jonah Hogate <Jonah.hogate@cox.net>
2021-08-20 21:26:25 -0500

    rendering nav

--- jonah-hogate-brainflix/src/App.js
+++ jonah-hogate-brainflix/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import './component/Nav/Nav';
+import BrainFlixLogo from './assets/Logo/Logo-brainflix.svg';
+import avitar from './assets/Images/Mohan-muruge.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <nav className="nav"> 
+        <img src={BrainFlixLogo} alt=''></img>
+        <form>
+            <textarea type='text' id='SearchBar' className='Nav__SearchBar' placeholder='Search'></textarea>
+        </form>
+        <button>+ UPLOAD</button>
+        <img src={avitar} alt='' className='Nav__Avitar'></img>
+      </nav>
     </div>
   );
 }
