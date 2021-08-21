7b6f985fe7e70feb88407d3dacc8c3bd2c49df3e
Jonah Hogate <Jonah.hogate@cox.net>
2021-08-21 00:03:13 -0500

    added video

--- jonah-hogate-brainflix/src/App.js
+++ jonah-hogate-brainflix/src/App.js
@@ -5,21 +5,21 @@ import NavRender from '../src/component/Nav/Nav'
 import { Component } from 'react';
 
 
-class App extends Component() {
+class App extends Component {
   state = {
 
   }
   
   render() {
-
+    return (
+      <div>
+      <NavRender></NavRender>
+      </div>
+    );
   }
 
 
-  return (
-    <div>
-    <NavRender></NavRender>;
-    </div>
-  );
+  
 };
 
 export default App;
