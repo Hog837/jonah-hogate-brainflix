a88cb8ac160b58135924cee27988eaa0acf5207a
Jonah Hogate <Jonah.hogate@cox.net>
2021-08-27 00:00:25 -0500

    finished styling video section for tablet

--- jonah-hogate-brainflix/src/component/Video/Video.js
+++ jonah-hogate-brainflix/src/component/Video/Video.js
@@ -1,6 +1,6 @@
 import './Video.scss';
 import '../../assets/Data/video-details.json';
-import someDude from '../../assets/Images/Mohan-muruge.jpg';
+
 
 function VideoRender({ selectedVideo }) {
     return (
@@ -18,38 +18,7 @@ function VideoRender({ selectedVideo }) {
                 </div>
                 <hr className='video__hr' />
             </div>
-                <p className='video__description' >{selectedVideo.description}</p>
-                {/* comments start here */}
-                <div className='comments'>
-                <p className='comments__length'>{selectedVideo.comments.length} Comments</p>
-                <div className='comments__add-comment' >
-                    <img className='comments__avitar' src= {someDude} alt=''/>
-                    <div className='comments__comment-container' >
-                        <p className='comments__join' >JOIN THE CONVERSATION</p>
-                        <textarea 
-                        className='comments__textarea'
-                        placeholder='Write comment here'/>
-                        <button className='comments__button' >COMMENT</button>
-                    </div>
-                </div>
-                <hr className='video__hr' />
-                {selectedVideo.comments.map( comment =>(
-                    <div key={selectedVideo.id}>
-                    <div className='comments__comment-comments'>
-                        <div className='comments__img'></div>
-                    <div className='comments__comment-container'>
-                        <div className='comments__name-timestamp' >
-                            <p className='comments__name' >{comment.name}</p>
-                            <p className='comments__timestamp' >{comment.timestamp}</p>
-                        </div>
-                        <p className='comments__comment' >{comment.comment}</p>
-                    </div>
-                    </div>
-                    <hr className='video__hr' />
-                </div>
-                )
-                )}
-                </div>
+            <p className='video__description' >{selectedVideo.description}</p>
         </section>
     );
 }
