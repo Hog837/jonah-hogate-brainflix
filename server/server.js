1b43b95383e540d18d1f3fb9badb7d5de9d4f325
Jonah Hogate <Jonah.hogate@cox.net>
2021-09-05 00:59:27 -0500

    adding routes for the rest API

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const videoRoutes = require('./routes/videoRoutes');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use(express.static('assets'));
+
+app.use(cors());
+
+app.use((_req, _res, next) => {
+    console.log('Incoming Request')
+    next();
+});
+
+app.use((req, res, next) => {
+    if (req.method === 'POST' && req.headers['content-type'] !== 'application/json') {
+    return res.status(400).send('Server requires application/json');
+    }
+    
+    console.log('Continue to the POST request');
+    next();
+});
+
+// app.get('/notes', (_req, res) => {
+//   res.send('Here are your notes and they are updated');
+// });
+
+app.use('/videos', videoRoutes);
+
+app. use('/videos/:id', videoRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running through ${PORT}`);
+});
